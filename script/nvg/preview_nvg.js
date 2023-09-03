2d432fa6c288e9c77ef5d68650636248560d711c
An Ngo <quocankg2611@gmail.com>
2023-09-03 21:44:20 +0700

    MCQ 4.0

--- script/nvg/preview_nvg.js
+++ script/nvg/preview_nvg.js
@@ -1,30 +1,3 @@
-function loadPreviewInfo() {
-    clearChildren(so_cau_moi_phan_div);
-    de_so_lb.textContent = "Đề số: " + currentTest_btn.getAttribute("de-so").toString();
-    thoi_gian_lb.textContent = "Thời gian làm bài: " + dataInfo.numPerTest.toString() + " phút";
-
-    const info_tong_lb = document.createElement('label');
-    info_tong_lb.textContent = "Tổng: " + dataInfo.numPerTest.toString() + " câu";
-    so_cau_moi_phan_div.appendChild(info_tong_lb);
-    if (Boolean(currentTest_btn.getAttribute("appendix")) == true) {
-        const info_ngau_nhien_lb = document.createElement('label');
-        info_ngau_nhien_lb.textContent = "Ngẫu nhiên: " + (dataInfo.numPerTest - dataInfo.appendixTestRepeatQuestion).toString() + " câu";
-        so_cau_moi_phan_div.appendChild(info_ngau_nhien_lb);
-        const info_lap_lai_lb = document.createElement('label');
-        info_lap_lai_lb.textContent = "Lặp lại: " + dataInfo.appendixTestRepeatQuestion.toString() + " câu";
-        so_cau_moi_phan_div.appendChild(info_lap_lai_lb);
-    } else {
-        for (let mcq_key in dataInfo.part) {
-            const info_mcq_lb = document.createElement('label');
-            info_mcq_lb.textContent = mcq_key.toString() + ": " + dataInfo.part[mcq_key].toString() + " câu";
-            so_cau_moi_phan_div.appendChild(info_mcq_lb);
-        }
-        const info_ngau_nhien_lb = document.createElement('label');
-        info_ngau_nhien_lb.textContent = "Ngẫu nhiên: " + dataInfo.appendixQuestion.toString() + " câu";
-        so_cau_moi_phan_div.appendChild(info_ngau_nhien_lb);
-    }
-}
-
 quaylai_preview_btn.addEventListener("click", () => {
     Preview_To_Pack();
 });
